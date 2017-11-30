dc08647d014c91c85b00a94ebb972b5a5e23d98f
god <164798848@qq.com>
2017-11-30 22:53:38 +0800

    code

---
+++ src/redux.tsx
@@ -0,0 +1,183 @@
+import {AnyAction, combineReducers} from "redux";
+import {isUndefined} from "util";
+import {DefaultApiFactory, LoginResponse, OAuth2AuthorizeParams} from "./api/account/gen/api";
+
+let accountApi=DefaultApiFactory(fetch,"http://127.0.0.1:8083/private-api/v1/accounts");
+
+export interface RootState {
+    loginResponse:LoginResponse
+}
+
+export interface ApiError {
+    status: number
+    code: string
+    message: string
+}
+
+const ON_ERROR_MESSAGE="ON_ERROR_MESSAGE";
+export function errorMessage(errorMessage:string) {
+    if(isUndefined(errorMessage)) {
+        console.error("errorMessage isUndefined");
+        errorMessage = "未知错误"
+    }
+
+    return {
+        type: ON_ERROR_MESSAGE,
+        error: true,
+        payload: {
+            errorMessage: errorMessage
+        },
+    }
+}
+
+function dispatchResponseError(dispatch:any,actionType:any,payload:any) {
+    dispatch({type: actionType, error: true, payload: payload});
+    dispatch(errorMessage(JSON.stringify(payload)))
+}
+
+function errorFromResponse(response:any):Promise<ApiError> {
+    if (response instanceof Response) {
+        return response.json().then((json: any) => {
+            return json
+        }).catch((err: any) => {
+            return {status: response.status, code: "NetworkException", message: err.toString}
+        })
+    } else if (response instanceof TypeError) {
+        if (response.message === "Failed to fetch") {
+            return new Promise(function (resolve) {
+                resolve({status: 8193, code: "NetworkException", message: "连接失败，请检查网络"})
+            });
+        } else {
+            return new Promise(function (resolve) {
+                resolve({status: 8193, code: "NetworkException", message: response.toString()})
+            });
+        }
+    } else {
+        return new Promise(function (resolve) {
+            resolve({status: 8193, code: "NetworkException", message: "未知错误 response:" + response})
+        });
+    }
+}
+
+export const SMS_CODE_REQUEST="SMS_CODE_REQUEST";
+export const SMS_CODE_SUCCESS="SMS_CODE_SUCCESS";
+export const SMS_CODE_FAILURE="SMS_CODE_FAILURE";
+export function apiSmsCode(params:{scene:string,phone:string,captchaId?:string,captchaCode?:string},
+                           onSuccess:()=>void,
+                           onError:(err:ApiError)=>void) {
+    console.log("apiSmsCode", params);
+    return function (dispatch: any) {
+        dispatch({type: SMS_CODE_REQUEST});
+        return accountApi.smsCode(params).then(() => {
+            dispatch({type: SMS_CODE_SUCCESS});
+            onSuccess()
+        }).catch((response) => {
+            errorFromResponse(response).then((err) => {
+                dispatchResponseError(dispatch, SMS_CODE_FAILURE, errorFromResponse(err));
+                onError(err)
+            })
+        })
+    }
+}
+
+export interface SmsSignupParams {
+    phone: string,
+    smsCode: string,
+    password: string,
+    oauth2AuthorizeParams?: OAuth2AuthorizeParams
+}
+export const SMS_SIGNUP_REQUEST="SMS_SIGNUP_REQUEST";
+export const SMS_SIGNUP_SUCCESS="SMS_SIGNUP_SUCCESS";
+export const SMS_SIGNUP_FAILURE="SMS_SIGNUP_FAILURE";
+export function apiSmsSignup(params:SmsSignupParams,
+                             onSuccess:(loginResponse:LoginResponse)=>void,
+                             onError:(err:ApiError)=>void) {
+    console.log("apiSmsSignup", params);
+    return function (dispatch: any) {
+        dispatch({type: SMS_SIGNUP_REQUEST});
+        return accountApi.smsSignup(params)
+            .then((data) => {
+                dispatch({type: SMS_SIGNUP_SUCCESS, payload: data});
+                onSuccess(data)
+            })
+            .catch((response) => {
+                errorFromResponse(response).then((err) => {
+                    dispatchResponseError(dispatch, SMS_SIGNUP_FAILURE, err);
+                    onError(err)
+                })
+            })
+    }
+}
+
+export interface SmsLoginParams {
+    phone: string,
+    smsCode: string,
+    oauth2AuthorizeParams?: OAuth2AuthorizeParams
+}
+export const SMS_LOGIN_REQUEST="SMS_LOGIN_REQUEST";
+export const SMS_LOGIN_SUCCESS="SMS_LOGIN_SUCCESS";
+export const SMS_LOGIN_FAILURE="SMS_LOGIN_FAILURE";
+export function apiSmsLogin(params:SmsLoginParams,
+                            onSuccess:(loginResponse:LoginResponse)=>void,
+                            onError:((err:ApiError)=>void)) {
+    console.log("apiSmsLogin", params);
+    return function (dispatch: any) {
+        dispatch({type: SMS_LOGIN_REQUEST});
+        return accountApi.smsLogin(params).then((data) => {
+            dispatch({type: SMS_LOGIN_SUCCESS, payload: data});
+            onSuccess(data)
+        }).catch((response) => {
+            errorFromResponse(response).then((err) => {
+                dispatchResponseError(dispatch, SMS_LOGIN_FAILURE, err);
+                onError(err)
+            })
+        })
+    }
+}
+
+export interface AccountLoginParams {
+    name: string,
+    password: string,
+    oauth2AuthorizeParams?: OAuth2AuthorizeParams
+}
+export const ACCOUNT_LOGIN_REQUEST="ACCOUNT_LOGIN_REQUEST";
+export const ACCOUNT_LOGIN_SUCCESS="ACCOUNT_LOGIN_SUCCESS";
+export const ACCOUNT_LOGIN_FAILURE="ACCOUNT_LOGIN_FAILURE";
+export function apiAccountLogin(params:AccountLoginParams,
+                                onSuccess:(loginResponse:LoginResponse)=>void,
+                                onError:((err:ApiError)=>void)) {
+    console.log("apiAccountLogin", params);
+    return function (dispatch: any) {
+        dispatch({type: ACCOUNT_LOGIN_REQUEST});
+        return accountApi.login(params).then((data) => {
+            dispatch({type: ACCOUNT_LOGIN_SUCCESS, payload: data});
+            onSuccess(data)
+        }).catch((response) => {
+            errorFromResponse(response).then((err) => {
+                dispatchResponseError(dispatch, ACCOUNT_LOGIN_FAILURE, err);
+                onError(err)
+            })
+        })
+    }
+}
+
+function loginResponse(state:LoginResponse,action:AnyAction) {
+    if (isUndefined(state)) {
+        return {}
+    }
+
+    switch (action.type) {
+        case SMS_SIGNUP_SUCCESS:
+            return action.payload;
+        case ACCOUNT_LOGIN_SUCCESS:
+            return action.payload;
+        case SMS_LOGIN_SUCCESS:
+            return action.payload;
+        default:
+            return state
+    }
+}
+
+export const rootReducer=combineReducers({
+    loginResponse
+});
