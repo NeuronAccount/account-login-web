3fd4806f1b0ae1ad58413b7aaa46dd3a4b0a37c8
god <164798848@qq.com>
2018-03-22 23:54:33 +0800

    code

--- _release.sh
+++ _release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-NAME="neuron-account-login-web" \
-VERSION="v1.0.0" \
+NAME=neuron-account-login-web \
+VERSION=v1.0.0 \
 PORT=3001 \
-PUBLIC_URL=/web/accounts/login \
+REACT_APP_WEB_PATH=/web/accounts/login \
 neuron-react-release.sh
